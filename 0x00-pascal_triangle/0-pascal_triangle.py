482247645de3986863622937e84e30b03754be41
KingHawkins <Kinghawkins61@gmail.com>
2023-06-27 10:40:54 +0300

    first interview question

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        (array, fib) = ([], [[1]])
+        while len(fib) < n:
+            (arr, item) = ([1], fib[-1])
+            for i in range(0, len(item)):
+                if i+1 != len(item):
+                    arr.append(item[i] + item[i + 1])
+            arr.append(1)
+            fib.append(arr)
+        return fib
