1ea09ac1b5354349beaf8f6b433cbba440c82728
KingHawkins <Kinghawkins61@gmail.com>
2023-07-13 20:02:28 +0300

    pushing all tasks

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+0-minimum operations.
+"""
+
+
+def minOperations(n):
+    """
+    Implememtation.
+    """
+    a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+        b += 1
+    return a
