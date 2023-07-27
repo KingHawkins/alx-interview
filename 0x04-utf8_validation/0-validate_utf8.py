d0e8470a66685000d39f20faf1f4a65f9800d8ff
KingHawkins <Kinghawkins61@gmail.com>
2023-07-27 20:17:43 +0300

    push tasks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,17 @@ def validUTF8(data):
     """
     Implementation.
     """
-    if all([item <= 255 for item in data]):
-        return True
-    else:
-        return False
+    utf8valid = 0
+    for val in data:
+        byte = val & 255
+        if utf8valid:
+            if byte >> 6 != 2:
+                return False
+            utf8valid -= 1
+            continue
+        while (1 << abs(7 - utf8valid)) & byte:
+            utf8valid += 1
+        if utf8valid == 1 or utf8valid > 4:
+            return False
+        utf8valid = max(utf8valid - 1, 0)
+    return utf8valid == 0
